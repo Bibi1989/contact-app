c14b5bd27a415767a6c088966877473d2e8c9500
bibi1989 <bibiaremieye@gmail.com>
2020-02-04 18:28:39 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+// var createError = require('http-errors');
+import createError, { HttpError } from 'http-errors'
+import express, { Request, Response, NextFunction} from 'express';
+import cookieParser from 'cookie-parser';
+import logger from'morgan';
+
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req: Request, res: Response, next: NextFunction) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
